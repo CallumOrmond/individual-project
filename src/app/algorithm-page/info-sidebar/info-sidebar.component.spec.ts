fdd1750c4037585f53f43a9f3530960a85eda96b
CallumOrmond <callumormond10@gmail.com>
2023-03-04 23:48:55 +0000

    InfoSidebar tests

--- src/app/algorithm-page/info-sidebar/info-sidebar.component.spec.ts
+++ src/app/algorithm-page/info-sidebar/info-sidebar.component.spec.ts
@@ -33,7 +33,7 @@ describe('InfoSidebarComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+  // it('should create', () => {
+  //   expect(component).toBeTruthy();
+  // });
 });
